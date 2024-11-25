9f49cab4b61423c882f48ed607bc4f4d43cc2281
ZegesMenden <camkullberg@gmail.com>
2024-11-25 18:50:24 -0500

    Update power.py
    Remove Battery class, as it has become too complicated for the scope of this simulation

--- src/power.py
+++ src/power.py
@@ -4,77 +4,102 @@ import sys
 import loguru
 from loguru import logger
 
-import time
-
-import liionpack as lp
-import pandas as pd
-import pybamm
-
-import common
 from .common import timedWrapper as timed
+from .common import initLogging as initLogging
 
-class BatteryParams:
-    Ri: float = 0.001
-    Rc: float = 0.001
-    Rb: float = 0.001
-    Rt: float = 0.001
+class SolarPannel:
 
-class SatPower:
+    def __init__(self, iMpp: float, vMpp: float, area: float, efficiency: float, temperatureCoeff: float, referenceTemp: float) -> None:
 
-    def __init__(self, parallelCells: int = 1, seriesCells: int = 1, cellVoltage: float = 3.7, battery: BatteryParams = BatteryParams()) -> None:
+        def castCheck(val: any, cast: type, name: str) -> any:
+            try: return cast(val)
+            except (ValueError, TypeError): raise TypeError(f"{name} must be a {cast}-like object")
+            except Exception as e: raise e
+
+        self._iMpp: float = castCheck(iMpp, float, "iMpp")
+        self._vMpp: float = castCheck(vMpp, float, "vMpp")
+        self._area: float = castCheck(area, float, "area")
+        self._efficiency: float = castCheck(efficiency, float, "efficiency")
+        self._temperatureCoeff: float = castCheck(temperatureCoeff, float, "temperatureCoeff")
+        self._referenceTemp: float = castCheck(referenceTemp, float, "referenceTemp")
+
+    @property
+    def area(self) -> float:
+        return self._area
+    
+    @property
+    def efficiency(self) -> float:
+        return self._efficiency
+    
+    @property
+    def temperatureCoeff(self) -> float:
+        return self._temperatureCoeff
+    
+    @property
+    def referenceTemp(self) -> float:
+        return self._referenceTemp
+    
+    def getPower(self, irradiance: float, temperature: float) -> float:
         """_summary_
 
         Args:
-            parallelCells (int, optional): _description_. Defaults to 1.
-            seriesCells (int, optional): _description_. Defaults to 1.
-            cellVoltage (float, optional): _description_. Defaults to 3.7.
-            Ri (float, optional): _description_. Defaults to 0.001.
-            Rc (float, optional): _description_. Defaults to 0.001.
-            Rb (float, optional): _description_. Defaults to 0.001.
-            Rt (float, optional): _description_. Defaults to 0.001.
+            irradiance (float): _description_
+            temperature (float): _description_
+
+        Returns:
+            float: _description_
         """
+        return irradiance * self._area * self._efficiency * (1 + self._temperatureCoeff * (temperature - self._referenceTemp))
 
-        def typeCheck(val: any, types: tuple[type], name: str) -> any:
-            if not isinstance(val, types): raise TypeError(f"{name} must be of type {types}")
-            return val
+    def maxPower(self) -> float:
+        return self._iMpp * self._vMpp
+    
+    def idealCurrent(self) -> float:
+        return self._iMpp
+    
+    def idealVoltage(self) -> float:
+        return self._vMpp
 
-        def castCheck(val: any, t: type, name: str) -> any:
-            try: val = t(val)
-            except (ValueError, TypeError): raise TypeError(f"{name} must be a {t}-like object")
-            return t(val)
-        
-        self._parallelCells = castCheck(parallelCells, int, "parallelCells")
-        self._seriesCells = castCheck(seriesCells, int, "seriesCells")
+class SolarArray:
 
-        self._batteryParams = typeCheck(battery, BatteryParams, "battery")
-        self._batteryParams.Ri = castCheck(self._batteryParams.Ri, float, "battery.Ri")
-        self._batteryParams.Rc = castCheck(self._batteryParams.Rc, float, "battery.Rc")
-        self._batteryParams.Rb = castCheck(self._batteryParams.Rb, float, "battery.Rb")
-        self._batteryParams.Rt = castCheck(self._batteryParams.Rt, float, "battery.Rt")
-        
-        if self._seriesCells < 1:
-            raise ValueError("Series cells must be greater than 0")
-        
-        if self._parallelCells < 1:
-            raise ValueError("Parallel cells must be greater than 0")
-        
-        initialVoltage = castCheck(cellVoltage, float, "cellVoltage")
-        
-        self._circuit: pd.DataFrame = lp.setup_circuit(Np=self._parallelCells, 
-                                                       Ns=self._seriesCells, 
-                                                       V=initialVoltage, 
-                                                       Ri=self._batteryParams._Ri, 
-                                                       Rc=self._batteryParams._Rc, 
-                                                       Rb=self._batteryParams._Rb, 
-                                                       Rt=self._batteryParams._Rt)
+    def __init__(self, panel: SolarPannel, parallelPanels: int, seriesPanels: int) -> None:
 
-    @property
-    def parallelCells(self) -> int:
-        return self._parallelCells
+        def castCheck(val: any, cast: type, name: str) -> any:
+            try: return cast(val)
+            except (ValueError, TypeError): raise TypeError(f"{name} must be a {cast}-like object")
+            except Exception as e: raise e
+
+        self._parallelPanels: int = castCheck(parallelPanels, int, "parallelPanels")
+        self._seriesPanels: int = castCheck(seriesPanels, int, "seriesPanels")
+
+        if self._parallelPanels < 1:
+            raise ValueError("Parallel panels must be greater than 0")
+        
+        if self._seriesPanels < 1:
+            raise ValueError("Series panels must be greater than 0")
+
+        if not isinstance(panel, SolarPannel):
+            raise TypeError("Panel must be of type SolarPannel")
+        
+        self._panel: SolarPannel = panel
 
     @property
-    def seriesCells(self) -> int: return self._seriesCells
+    def panel(self) -> SolarPannel:
+        return self._panel
     
     @property
-    def batteryParams(self) -> BatteryParams:
-        return self._batteryParams
+    def parallelPanels(self) -> int:
+        return self._parallelPanels
+    
+    @property
+    def seriesPanels(self) -> int:
+        return self._seriesPanels
+
+    def getPower(self, irradiance: float, temperature: float) -> float:
+        return self._panel.getPower(irradiance, temperature) * self._parallelPanels * self._seriesPanels
+    
+    def getVoltage(self, irradiance: float, temperature: float) -> float:
+        return self.getPower(irradiance, temperature) / self._panel.idealCurrent()
+    
+    def getCurrent(self, irradiance: float, temperature: float) -> float:
+        return self.getPower(irradiance, temperature) / self._panel.idealVoltage()
